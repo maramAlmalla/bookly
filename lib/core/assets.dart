85bad039cd7a2ae91bc230d74ff491a3422cacb3
maram <maramalm367@gmail.com>
2024-10-24 11:54:12 +0300

    add images & create custom list view items

--- lib/core/assets.dart
+++ lib/core/assets.dart
@@ -1,3 +1,4 @@
 class AssetsData {
   static const logo = "lib/assets/images/logo.png";
+  static const book1 = "lib/assets/images/book1.png";
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/assets.dart';
 import 'package:bookly/features/home/presentation/views/widgets/Custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -10,7 +11,34 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(title: 'BOOKLY', icon: FontAwesomeIcons.magnifyingGlass),
+        SizedBox(
+          height: 20,
+        ),
+        CustomListViewItems(),
       ],
+    );
+  }
+}
+
+class CustomListViewItems extends StatelessWidget {
+  const CustomListViewItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .25,
+      child: AspectRatio(
+        aspectRatio: 2.28 / 3,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.black,
+              image: const DecorationImage(
+                image: AssetImage(AssetsData.book1),
+                fit: BoxFit.fill,
+              )),
+        ),
+      ),
     );
   }
 }
