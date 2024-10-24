4efb4a13f18ba664299b2103dafd7c2ae2ac444a
maram <maramalm367@gmail.com>
2024-10-24 13:58:22 +0300

    add text theme

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/core/assets.dart';
 import 'package:bookly/core/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/Custom_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/Featured_List_view_items.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_items.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -23,25 +24,6 @@ class HomeViewBody extends StatelessWidget {
           ),
         )
       ],
-    );
-  }
-}
-
-class FeaturedBooksListItems extends StatelessWidget {
-  const FeaturedBooksListItems({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListViewItems(),
-            );
-          }),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/constant.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(scaffoldBackgroundColor: KPrimaryColor),
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: KPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(),
+      ),
       home: const SplashView(),
     );
   }
